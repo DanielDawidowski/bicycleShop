179493bb9f13ee712d37d9d8c22588aec4653aa6
Daniel Dawidowski <dvds1987@gmail.com>
2019-10-11 13:24:14 +0200

    Adding basic file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
+import { FaGulp } from 'react-icons/fa'
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -12,9 +13,11 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/">Go </Link>
+
+    <FaGulp/>
+    <div>Icons made by <a href="https://www.flaticon.com/authors/photo3idea-studio" title="photo3idea_studio">photo3idea_studio</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a></div>
   </Layout>
 )
 
